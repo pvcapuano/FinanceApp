3825870e283718b4546152adeaa02c833797331b
Paulo Victor Capuano <pvcapuano@gmail.com>
2022-11-15 19:58:44 -0300

    add icons on form

--- layout/layout.jsx
+++ layout/layout.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
     <div className="flex h-screen bg-gray-700">
-      <div className="m-auto bg-slate-50 rounded-md w-2/4 h-3/4 grid lg:grid-cols-1">
-        <div className="right flex flex-col justify-evenly">
+      <div className="m-auto bg-slate-50 rounded-md w-2/4 md:w-1/3 h-4/4 grid lg:grid-cols-1">
+        <div className="right flex flex-col justify-between">
           <div className="text-center py-10">{children}</div>
         </div>
       </div>
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Head from "next/head";
 import Layout from "../layout/layout";
 import Link from "next/link";
+import styles from "../styles/Form.module.css";
+import Image from "next/image";
+import { HiAtSymbol, HiOutlineFingerPrint } from "react-icons/hi";
 
 const Login = () => {
+  const [show, setShow] = useState(false);
   return (
     <Layout>
       <Head>
@@ -16,25 +20,54 @@ const Login = () => {
         </div>
 
         <form className="flex flex-col gap-5">
-          <div className="input-group">
-            <input type="email" name="email" placeholder="E-mail" />
+          <div className={styles.input_group}>
+            <input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              className={styles.input_text}
+            />
+            <span className="icon flex items-center px-4">
+              <HiAtSymbol size={25} />
+            </span>
           </div>
-          <div className="input-group">
-            <input type="password" name="password" placeholder="Senha" />
+          <div className={styles.input_group}>
+            <input
+              type={`${show ? "text" : "password"}`}
+              name="password"
+              placeholder="Senha"
+              className={styles.input_text}
+            />
+            <span
+              className="icon flex items-center px-4"
+              onClick={() => setShow(!show)}
+            >
+              <HiOutlineFingerPrint size={25} />
+            </span>
           </div>
 
           <div className="input-button">
-            <button type="submit">Login</button>
+            <button type="submit" className={styles.button}>
+              Login
+            </button>
           </div>
           <div className="input-button">
-            <button type="submit">Login com Google</button>
+            <button type="button" className={styles.button_custom}>
+              Login com Google
+              <Image
+                alt="google"
+                src={"/assets/google.png"}
+                width="20"
+                height={20}
+              ></Image>
+            </button>
           </div>
         </form>
 
         <p className="text-center text-gray-400">
-          Não tem uma conta?{" "}
+          Não tem uma conta?
           <Link legacyBehavior href={"/register"}>
-            <a className="text-blue-700">Cadastre-se</a>
+            <a className="text-blue-700"> Cadastre-se</a>
           </Link>
         </p>
       </section>
