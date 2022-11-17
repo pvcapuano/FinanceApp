3d18b7f8803314d1c4510cf941c3d579313e028c
Paulo Victor Capuano <pvcapuano@gmail.com>
2022-11-17 19:08:44 -0300

    login validate

--- pages/login.tsx
+++ pages/login.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { HiAtSymbol, HiOutlineFingerPrint } from "react-icons/hi";
 import { signIn, signOut } from "next-auth/react";
 import { useFormik, FormikProps } from "formik";
+import loginValidate from "../lib/validate";
 
 interface FormValues {
   email: string;
@@ -21,8 +22,11 @@ const Login = () => {
       email: "",
       password: "",
     },
+    validate: loginValidate,
     onSubmit: onSubmit,
   });
+
+  console.log(formik.errors);
 
   async function onSubmit(values: any) {
     console.log(values);
@@ -57,6 +61,11 @@ const Login = () => {
               <HiAtSymbol size={25} />
             </span>
           </div>
+          {formik.errors.email && formik.touched.email ? (
+            <span className="text-rose-500 text-xs">{formik.errors.email}</span>
+          ) : (
+            <></>
+          )}
           <div className={styles.input_group}>
             <input
               type={`${show ? "text" : "password"}`}
@@ -73,7 +82,13 @@ const Login = () => {
               <HiOutlineFingerPrint size={25} />
             </span>
           </div>
-
+          {formik.errors.password && formik.touched.password ? (
+            <span className="text-rose-500 text-xs">
+              {formik.errors.password}
+            </span>
+          ) : (
+            <></>
+          )}
           <div className="input-button">
             <button type="submit" className={styles.button}>
               Login
